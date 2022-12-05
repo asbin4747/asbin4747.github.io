0fc42f0ce766d253477e9cfac1a79f87514ec77b
cetaldeve <asbin.dahal4747@gmail.com>
2022-12-04 22:54:07 -0600

    Removed git config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
-  assetPrefix: isProd ? "/aboutme/" : "",
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     unoptimized: true,
