96bbaa123e5034d5f67855b55f30da4fc7948054
cetaldeve <asbin.dahal4747@gmail.com>
2022-12-05 17:21:34 -0600

    Added akamai loader for nextjs

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    unoptimized: true,
+    loader: "akamai",
+    path: "",
+
   },
   reactStrictMode: true,
 };
