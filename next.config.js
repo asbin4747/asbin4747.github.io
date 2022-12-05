5a7e0b28cbd5e22fb5a776c3a9d4eb6c6f291318
cetaldeve <asbin.dahal4747@gmail.com>
2022-12-04 22:25:01 -0600

    Added config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
+  assetPrefix: isProd ? '/aboutme/' : '',
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    unoptimized: true,
   },
   reactStrictMode: true,
 }
